9318b085e09dd12ebdb94f1d628f04876a571975
Kytech <Kytech@users.noreply.github.com>
2023-01-15 00:40:52 -0700

    Create generic wrapper for primitive returns of GetFirmwareEnvironmentVariable.

--- Firmware.cs
+++ Firmware.cs
@@ -13,7 +13,7 @@ namespace BootMan
             FirmwareTypeMax,
         }
 
-        private static readonly string EFI_GLOBAL_VARIABLE = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
+        public static readonly string EFI_GLOBAL_VARIABLE = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}";
 
         [DllImport("Kernel32.dll")]
         private static extern bool GetFirmwareType(out FirmwareType firmwareType);
@@ -22,7 +22,7 @@ namespace BootMan
         private static extern uint GetFirmwareEnvironmentVariable(
             string lpName,
             string lpGuid,
-            Span<ushort> lpBuffer,
+            byte[] lpBuffer,
             uint nSize
         );
 
@@ -30,13 +30,25 @@ namespace BootMan
         private static extern bool SetFirmwareEnvironmentVariable(
             string lpName,
             string lpGuid,
-            Span<ushort> lpBuffer,
+            byte[] lpBuffer,
             uint nSize
         );
 
+        public static unsafe T GetFirmwareEnvironmentVariable<T>(string name, string guid) where T : unmanaged
+        {
+            var buffer = new byte[sizeof(T)];
+            uint size = GetFirmwareEnvironmentVariable(name, guid, buffer, (uint)buffer.Length);
+            if (size == 0)
+            {
+                throw new Exception($"GetFirmwareEnvironmentVariable failed with error code {Marshal.GetLastPInvokeError()}");
+            }
+            return MemoryMarshal.Cast<byte, T>(buffer)[0];
+        }
+
         public static FirmwareType GetFirmwareType()
         {
-            bool success = Firmware.GetFirmwareType(out FirmwareType firmwareType);
+            // TODO: Do error handling to indicate win32 error
+            bool success = GetFirmwareType(out FirmwareType firmwareType);
             return success ? firmwareType : FirmwareType.FirmwareTypeUnknown;
         }
     }
